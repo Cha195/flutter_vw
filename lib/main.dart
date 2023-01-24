53b69847bd7c727f5064e96e3c5ab2bd7618ba2b
Sricharan Ramesh <sricharan.ramesh@pocketpills.com>
2023-01-24 10:48:35 +0530

    feat: initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const MyHomePage(title: 'Flutter Demo Home Page'),
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: const PocketPillsWV(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+    ));
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class PocketPillsWV extends StatefulWidget {
+  const PocketPillsWV({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  PocketPillsWVState createState() => PocketPillsWVState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class PocketPillsWVState extends State<PocketPillsWV> {
+  // final Set<String> _favorites = <String>{};
+  final WebViewController _controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://testapp.pocketpills.com/?debug=true'));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: null,
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: _controller,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: _bookmarkButton(),
     );
   }
 }
