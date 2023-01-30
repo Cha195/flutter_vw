c99b4933107e8ed4bfb90d7ba5c9509951ef10e8
Sricharan Ramesh <sricharan.ramesh@pocketpills.com>
2023-01-30 12:13:17 +0530

    feat: add phone dailer and mail function to navigationDelegate

---
+++ lib/PageNotFound.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class PageNotFound extends StatelessWidget {
+  const PageNotFound({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: const Color(0xffd8f3dc),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 150,
+            bottom: 0,
+            left: 24,
+            right: 24,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  '404',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 50,
+                      letterSpacing: 2,
+                      color: Color(0xff2f3640),
+                      fontFamily: 'Anton',
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Sorry, we couldn\'t find the page!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xff2f3640),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:fl_web_view/PageNotFound.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MaterialApp(
@@ -14,38 +16,81 @@ class PocketPillsWV extends StatefulWidget {
 }
 
 class PocketPillsWVState extends State<PocketPillsWV> {
-  // final Set<String> _favorites = <String>{};
-  final WebViewController _controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar.
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
+  late final WebViewController _controller;
+  double progressValue = 0;
+  bool isLoading = true;
+  bool isError = false;
+
+  _launchURL(Uri uri) async {
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $Uri';
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(onProgress: (int progress) {
+          setState(() {
+            progressValue = progress / 100;
+          });
+        }, onPageStarted: (String url) {
+          setState(() {
+            isLoading = true;
+          });
+        }, onPageFinished: (String url) {
+          setState(() {
+            isLoading = false;
+          });
+        }, onWebResourceError: (WebResourceError error) {
+          setState(() {
+            isError = true;
+          });
+        }, onNavigationRequest: (NavigationRequest request) async {
+          if (request.url.contains("mailto:") || request.url.contains("tel:")) {
+            final splitUrl = request.url.split(':');
+            final uri = Uri(
+              scheme: splitUrl[0],
+              path: '+${splitUrl[1]}',
+            );
+            _launchURL(uri);
             return NavigationDecision.prevent;
           }
           return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..enableZoom(false) // DISABLE ZOOM TO PREVENT AUTO-ZOOM ON INPUT FOCUS
-    ..loadRequest(Uri.parse('https://testapp.pocketpills.com/?debug=true'));
+        }),
+      )
+      ..enableZoom(false) // DISABLE ZOOM TO PREVENT AUTO-ZOOM ON INPUT FOCUS
+      ..loadRequest(Uri.parse('https://testapp.pocketpills.com/?debug=true'));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: SafeArea(
-        child: WebViewWidget(
-          controller: _controller,
-        ),
-      ),
-      // floatingActionButton: _bookmarkButton(),
+      body: isError
+          ? const PageNotFound()
+          : isLoading
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: LinearProgressIndicator(
+                        value: progressValue,
+                      ),
+                    ),
+                  ],
+                )
+              : SafeArea(
+                  child: WebViewWidget(
+                    controller: _controller,
+                  ),
+                ),
     );
   }
 }
